c9d139552f5321ec197e159fab8edf6c03dd33fb
Rualito <raulfilipe1998@hotmail.com>
2020-04-29 22:15:43 +0100

    save

--- spectrum-analysis.py
+++ spectrum-analysis.py
@@ -1,8 +1,46 @@
 import subprocess
 
-def plotFile(filename):
-    pass
+# https://stackoverflow.com/questions/2161932/calling-gnuplot-from-python#2161965
 
+def plotFile(filename):
+    name, ext = filename.split(",", 2)
+    if ext != "txt":
+        print("Wrong file format, returning")
+        return
+
+    data = getTxtFileArray(filename)
+
+    tempname = "tempfile.dat"
+
+    generateTempGnuplotFile(tempname, data)
+
+
+
+
+def getTxtFileArray(filename):
+    # assuming first line is useless
+    file = open(filename, "r")
+
+    lines = file.readlines()
+    file.close()
+
+    arr = []
+
+    for i in range(1, len(lines)):
+        arr.append(lines[i].split())
+
+    return arr
+
+def generateTempGnuplotFile(filename, array):
+    file = open(filename, "w")
+
+    for e in array:
+        file.write(' '.join(e))
+
+    file.close()
 
 def xryToTxt(filename):
     pass
+
+
+print(' '.join(str(e) for e in [1, 2, 3]))
