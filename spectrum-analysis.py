9bacc4b934a6363420ebb2c9d671e696bd189d37
Rualito <raulfilipe1998@hotmail.com>
2020-04-29 22:55:52 +0100

    save

--- spectrum-analysis.py
+++ spectrum-analysis.py
@@ -1,6 +1,5 @@
-import subprocess
-
-# https://stackoverflow.com/questions/2161932/calling-gnuplot-from-python#2161965
+import PyGnuplot as gp
+import numpy as np
 
 def plotFile(filename):
     name, ext = filename.split(",", 2)
@@ -12,12 +11,10 @@ def plotFile(filename):
 
     tempname = "tempfile.dat"
 
-    generateTempGnuplotFile(tempname, data)
+    gp.s(data, tempname)
 
 
-
-
-def getTxtFileArray(filename):
+def getTxtFileArray(filename, n):
     # assuming first line is useless
     file = open(filename, "r")
 
@@ -25,22 +22,41 @@ def getTxtFileArray(filename):
     file.close()
 
     arr = []
+    for i in range(n):
+        arr.append([])
 
-    for i in range(1, len(lines)):
-        arr.append(lines[i].split())
+    for i in range(len(lines)):
+        vals = []
+        try:
+            strVal = lines[i].split()
+
+            for str in strVal:
+                # converting all the strings into values, with the try checking for any incompatible value
+                vals.append(float(str))
+
+            for j in range(len(vals)):
+                #loading the values onto the array
+                arr[j].append(vals[j])
+        except ValueError:
+            continue
+
 
     return arr
-
-def generateTempGnuplotFile(filename, array):
-    file = open(filename, "w")
-
-    for e in array:
-        file.write(' '.join(e))
-
-    file.close()
 
 def xryToTxt(filename):
     pass
 
-
 print(' '.join(str(e) for e in [1, 2, 3]))
+
+
+arrX = []
+arrY = []
+for i in range(5):
+    arrX.append(i)
+    arrY.append(i**2)
+
+gp.s([arrX, arrY], "temp.dat")
+#gp.c('plot "temp.dat"')
+
+
+print(int("3.1"))
