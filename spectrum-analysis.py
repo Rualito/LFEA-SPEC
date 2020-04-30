c6ae76d46dd63b095e7341632ccf98dc89768f77
Rualito <raulfilipe1998@hotmail.com>
2020-04-30 10:42:12 +0100

    xry to txt update

--- spectrum-analysis.py
+++ spectrum-analysis.py
@@ -1,21 +1,24 @@
 import PyGnuplot as gp
 import numpy as np
 
-def plotFile(filename):
-    name, ext = filename.split(",", 2)
-    if ext != "txt":
+from os import  listdir
+from os.path import isfile, join, splitext
+from decimal import Decimal
+
+def plotFile(filename, n):
+    name, ext = splitext(filename)
+    if ext != ".txt":
         print("Wrong file format, returning")
         return
 
-    data = getTxtFileArray(filename)
+    data = getTxtFileArray(filename, n)
 
     tempname = "tempfile.dat"
 
     gp.s(data, tempname)
-
+    gp.c("plot 'tempfile.dat' u 1:2 w lp")
 
 def getTxtFileArray(filename, n):
-    # assuming first line is useless
     file = open(filename, "r")
 
     lines = file.readlines()
@@ -34,30 +37,57 @@ def getTxtFileArray(filename, n):
                 # converting all the strings into values, with the try checking for any incompatible value
                 vals.append(float(str))
 
-            for j in range(len(vals)):
-                #loading the values onto the array
+            for j in range(n):
+                # loading the values onto the array
                 arr[j].append(vals[j])
         except ValueError:
             continue
 
     return arr
 
-
 def xryToTxt(filename):
-    pass
+    name, ext = splitext(filename)
+
+    if ext != ".xry":
+        print("Wrong file extension, returning ", name, ext)
+        return False
+
+    fileR = open(filename, "r")
+
+    lines = fileR.readlines()
+    fileR.close()
+
+    sAng = Decimal(lines[4].split()[0])
+    step = Decimal(lines[4].split()[3])
+
+    sL = 18
+    fL = sL+int(lines[17].split()[1])
+
+    fileW = open(name+".txt", "w")
+
+    for i in range(fL-sL):
+        fileW.write(str(sAng+i*step) + " " + ' '.join(lines[sL+i].split())+"\n")
+
+    fileW.close()
+    return True
+
+mainDataDir = "SPEC"
+
+def convertXRYDirToTxt(dir):
+    xryNames = [f for f in listdir(dir) if isfile(join(dir, f)) and splitext(join(dir, f))[1]=='.xry']
+
+    fullPathNames = [join(dir, name) for name in xryNames]
+    [xryToTxt(fullName) for fullName in fullPathNames]
+
+def convertAll():
+    convertXRYDirToTxt("SPEC\\2D\\2D data files")
+    convertXRYDirToTxt("SPEC\\3D\\3D data files\\1seccao")
+    convertXRYDirToTxt("SPEC\\3D\\3D data files\\2seccao")
+    convertXRYDirToTxt("SPEC\\4D\\4D data files")
+    convertXRYDirToTxt("SPEC\\5D\\5D data files")
+
+def __main__():
+    convertAll()
 
 
-print(' '.join(str(e) for e in [1, 2, 3]))
-
-
-arrX = []
-arrY = []
-for i in range(5):
-    arrX.append(i)
-    arrY.append(i**2)
-
-gp.s([arrX, arrY], "temp.dat")
-#gp.c('plot "temp.dat"')
-
-
-print(int("3.1"))
+__main__()
